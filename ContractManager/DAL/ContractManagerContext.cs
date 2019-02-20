f5966b7164c6c8ac70f706818336e97b9320dcfe
John Lee <boblee167@gmail.com>
2019-02-20 20:41:42 +0000

    Getting Started - 1st commit

---
+++ ContractManager/DAL/ContractManagerContext.cs
@@ -0,0 +1,29 @@
+﻿using ContractManager.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ContractManager.DAL
+{
+    public class ContractManagerContext : IdentityDbContext<ApplicationUser>
+    {
+        public ContractManagerContext() : base("ContractManagerContext")
+        {
+        }
+
+        public DbSet<Supplier> CMSupplier { get; set; }
+        public DbSet<Property> CMProperty { get; set; }
+        public DbSet<ContractDetails> CMContract { get; set; }
+        public DbSet<ContractHours> CMCleaningHours { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
+
---
+++ ContractManager/Models/ContractHours.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContractManager.Models
+{
+    public enum WeekDay {Mon,Tue,Wed,Thu,Fri,Sat,Sun}
+        
+
+    public class ContractHours
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Transaction_ID { get; set; }
+
+        [ForeignKey("Property")]
+        public int Property_ID { get; set; }
+
+        [ForeignKey("ContractDetails")]
+        public int Contract_ID { get; set; }
+
+        [Required]
+        public WeekDay Weekday { get; set; }
+                                              
+        [Required]
+        public double DailyHours {get; set; }
+
+        public DateTime HoursUpdatedDate { get; set; }
+
+        public double WeeklyHours { get; set; }
+
+        public double AvgMonthlyHours { get; set; }
+
+
+        // Navigation Properties
+        public virtual Property Property { get; set; }
+        public virtual ContractDetails ContractDetails { get; set; }
+    }
+
+    
+
+}
